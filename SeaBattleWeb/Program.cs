acc1d96232278666c8560dfd3a12a9510ea9b237
Flare <vmail5249@gmail.com>
2024-04-29 20:40:24 +0500

    Find configuration by AppDomain.CurrentDomain.BaseDirectory

--- SeaBattleWeb/Program.cs
+++ SeaBattleWeb/Program.cs
@@ -7,9 +7,9 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
         if (builder.Environment.IsProduction())
         {
-            string configPath = $"{builder.Environment.ContentRootPath}/appsettings.json";
+            string configPath = $"{AppDomain.CurrentDomain.BaseDirectory}/appsettings.json";
             Console.WriteLine($"Add configuration {configPath}");
-            builder.Configuration.AddJsonFile($"{configPath}");
+            builder.Configuration.AddJsonFile(configPath);
         }
         var startup = new Startup(builder.Configuration);
         startup.ConfigureServices(builder.Services);
