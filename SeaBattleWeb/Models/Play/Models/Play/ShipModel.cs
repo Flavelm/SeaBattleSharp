93bad3e284830b90a4a25b985fbe6e6b9bed7d6a
Flare <vmail5249@gmail.com>
2024-05-16 22:53:52 +0500

    idk

--- SeaBattleWeb/Models/Play/Models/Play/ShipModel.cs
+++ SeaBattleWeb/Models/Play/Models/Play/ShipModel.cs
@@ -1,9 +1,12 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SeaBattleWeb.Models.Play.Models.Play;
 
 public class ShipModel(int x, int y) : PositionModel(x, y)
 {
+    [ForeignKey("Id")] public int Id { get; init; }
+    
     public event EventHandler Updated; 
     public bool IsBroken { get; set; }
 
