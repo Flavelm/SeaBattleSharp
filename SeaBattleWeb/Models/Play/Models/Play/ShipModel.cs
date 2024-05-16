8d9843b6e6316b47c9cb5e0babdf4fc391420c79
Flare <vmail5249@gmail.com>
2024-05-16 22:49:33 +0500

    Remove id from ships

--- SeaBattleWeb/Models/Play/Models/Play/ShipModel.cs
+++ SeaBattleWeb/Models/Play/Models/Play/ShipModel.cs
@@ -4,8 +4,6 @@ namespace SeaBattleWeb.Models.Play.Models.Play;
 
 public class ShipModel(int x, int y) : PositionModel(x, y)
 {
-    [Key] public int Id { get; init; }
-    
     public event EventHandler Updated; 
     public bool IsBroken { get; set; }
 
